ba79fbc7238dc40d20dc072e8501e5ef9d09cea2
Abhisheikh Gill <abhisheikhg@gmail.com>
2020-06-22 14:41:14 -0400

    Init commit, created Header, Footer, and LandingPage

---
+++ src/components/App.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { ThemeProvider } from "@material-ui/styles";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import theme from "./ui/Theme";
+import Header from "../components/ui/Header";
+import Footer from "../components/ui/Footer";
+import LandingPage from "./LandingPage";
+
+function App() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [value, setValue] = useState(0);
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header
+          setValue={setValue}
+          setSelectedIndex={setSelectedIndex}
+          value={value}
+          selectedIndex={selectedIndex}
+        />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={(props) => (
+              <LandingPage
+                {...props}
+                setValue={setValue}
+                setSelectedIndex={setSelectedIndex}
+              />
+            )}
+          />
+          <Route exact path="/services" component={() => <div>Services</div>} />
+          <Route
+            exact
+            path="/customsoftware"
+            component={() => <div>Custom Software</div>}
+          />
+          <Route
+            exact
+            path="/mobileapps"
+            component={() => <div>Mobile Apps</div>}
+          />
+          <Route exact path="/websites" component={() => <div>Websites</div>} />
+          <Route
+            exact
+            path="/revolution"
+            component={() => <div>Revolution</div>}
+          />
+          <Route exact path="/about" component={() => <div>About</div>} />
+          <Route exact path="/contact" component={() => <div>Contact</div>} />
+          <Route
+            exact
+            path="/estimates"
+            component={() => <div>Estimates</div>}
+          />
+        </Switch>
+        <Footer
+          value={value}
+          setValue={setValue}
+          selectedIndex={selectedIndex}
+          setSelectedIndex={setSelectedIndex}
+        />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/components/ui/Footer.js
@@ -0,0 +1,260 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import { Link } from "react-router-dom";
+import Hidden from "@material-ui/core/Hidden";
+
+import footerAdornment from "../../assets/Footer Adornment.svg";
+import facebook from "../../assets/facebook.svg";
+import twitter from "../../assets/twitter.svg";
+import instagram from "../../assets/instagram.svg";
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    backgroundColor: theme.palette.common.blue,
+    width: "100%",
+    zIndex: 1302,
+    position: "relative",
+  },
+  adornment: {
+    width: "25em",
+    verticalAlign: "bottom",
+    [theme.breakpoints.down("md")]: {
+      width: "21em",
+    },
+    [theme.breakpoints.down("sm")]: {
+      width: "15em",
+    },
+  },
+  mainContainer: {
+    position: "absolute",
+  },
+  link: {
+    color: "white",
+    fontFamily: "Arial",
+    fontSize: "0.75rem",
+    fontWeight: "bold",
+    textDecoration: "none",
+  },
+  gridItem: {
+    margin: "3em",
+  },
+  icon: {
+    height: "4em",
+    width: "4em",
+    [theme.breakpoints.down("xs")]: {
+      height: "2.5em",
+      width: "2.5em",
+    },
+  },
+  socialContainer: {
+    position: "absolute",
+    marginTop: "-6em",
+    right: "1.5em",
+    [theme.breakpoints.down("xs")]: {
+      right: "0.6em",
+    },
+  },
+}));
+
+export default function Footer(props) {
+  const classes = useStyles();
+  const { setValue, setSelectedIndex } = props;
+
+  return (
+    <footer className={classes.footer}>
+      <Hidden mdDown>
+        <Grid container justify="center" className={classes.mainContainer}>
+          <Grid item className={classes.gridItem}>
+            <Grid container direction="column" spacing={2}>
+              <Grid
+                item
+                className={classes.link}
+                onClick={() => setValue(0)}
+                component={Link}
+                to="/"
+              >
+                Home
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item className={classes.gridItem}>
+            <Grid container direction="column" spacing={2}>
+              <Grid
+                item
+                className={classes.link}
+                onClick={() => {
+                  setValue(1);
+                  setSelectedIndex(0);
+                }}
+                component={Link}
+                to="/services"
+              >
+                Services
+              </Grid>
+              <Grid
+                item
+                className={classes.link}
+                onClick={() => {
+                  setValue(1);
+                  setSelectedIndex(1);
+                }}
+                component={Link}
+                to="/software"
+              >
+                Custom Software Development
+              </Grid>
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                onClick={() => {
+                  setValue(1);
+                  setSelectedIndex(2);
+                }}
+                to="/mobileapps"
+              >
+                iOS/Android App Development
+              </Grid>
+              <Grid
+                item
+                className={classes.link}
+                onClick={() => {
+                  setValue(1);
+                  setSelectedIndex(3);
+                }}
+                component={Link}
+                to="/websites"
+              >
+                Website Development
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item className={classes.gridItem}>
+            <Grid container direction="column" spacing={2}>
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                onClick={() => setValue(2)}
+                to="/revolution"
+              >
+                The Revolution
+              </Grid>
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                onClick={() => setValue(2)}
+                to="/revolution"
+              >
+                Vision
+              </Grid>
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                onClick={() => setValue(2)}
+                to="/revolution"
+              >
+                Technology
+              </Grid>
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                onClick={() => setValue(2)}
+                to="/revolution"
+              >
+                Process
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item className={classes.gridItem}>
+            <Grid container direction="column" spacing={2}>
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                to="/about"
+                onClick={() => setValue(3)}
+              >
+                About Us
+              </Grid>
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                to="/about"
+                onClick={() => setValue(3)}
+              >
+                History
+              </Grid>
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                to="/about"
+                onClick={() => setValue(3)}
+              >
+                Team
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item className={classes.gridItem}>
+            <Grid container direction="column" spacing={2}>
+              <Grid
+                item
+                className={classes.link}
+                component={Link}
+                to="/contact"
+                onClick={() => setValue(4)}
+              >
+                Contact Us
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Hidden>
+      <img
+        alt="black decorate slash"
+        src={footerAdornment}
+        className={classes.adornment}
+      />
+      <Grid
+        container
+        justify="flex-end"
+        spacing={2}
+        className={classes.socialContainer}
+      >
+        <Grid
+          item
+          component={"a"}
+          href="https://www.facebook.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img alt="facebook logo" src={facebook} className={classes.icon} />
+        </Grid>
+        <Grid
+          item
+          component={"a"}
+          href="https://www.twitter.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img alt="twitter logo" src={twitter} className={classes.icon} />
+        </Grid>
+        <Grid
+          item
+          component={"a"}
+          href="https://www.instagram.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img alt="instagram logo" src={instagram} className={classes.icon} />
+        </Grid>
+      </Grid>
+    </footer>
+  );
+}
